c0e791370355fe37540e8ffed79f5d736f602e0a
jolan <mc-somango@live.fr>
2017-11-09 02:00:04 +0100

    Following of the previous commit

---
+++ cogs/coinmarketcap.py
@@ -0,0 +1,17 @@
+import requests
+from discord.ext import commands
+import json
+
+
+class CoinMarketCap:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="price")
+    async def get_price(self):
+        try :
+            r = requests.get("https://api.coinmarketcap.com/v1/ticker/ark/")
+            price = r.json()["price"]
+            await self.bot.say(price)
+        except commands.CommandError as e:
+            await self.bot.say("Command invalid.")
