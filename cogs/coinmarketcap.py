26c92cd014c32656de3a07785960b8f665d641e1
jolan <mc-somango@live.fr>
2017-11-09 02:12:52 +0100

    Fix

--- cogs/coinmarketcap.py
+++ cogs/coinmarketcap.py
@@ -9,9 +9,9 @@ class CoinMarketCap:
 
     @commands.command(name="price")
     async def get_price(self):
-        try :
+        try:
             r = requests.get("https://api.coinmarketcap.com/v1/ticker/ark/")
-            price = r.json()["price"]
+            price = r.json()[0]["price"]
             await self.bot.say(price)
         except commands.CommandError as e:
             await self.bot.say("Command invalid.")
